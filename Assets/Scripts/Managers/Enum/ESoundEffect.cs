e370f586eb0ff020f2d318c894a26f8fe2068cd8
Kamil <kamil.antos@qsystems.pro>
2024-04-07 13:15:25 +0200

    Changed truck

--- Assets/Scripts/Managers/Enum/ESoundEffect.cs
+++ Assets/Scripts/Managers/Enum/ESoundEffect.cs
@@ -2,7 +2,26 @@
 {
     public enum ESoundEffect
     {
+        BGM,
+        cage_open,
         cat_damage,
-        cat_jump
+        cat_unlocked,
+        deep_pluck,
+        drill,
+        enemy_attack,
+        env,
+        footsteps,
+        grunt,
+        happy_cats,
+        hit_effect,
+        laser,
+        level_up,
+        pluck,
+        poop,
+        purr,
+        run,
+        slash,
+        tail_sweep,
+        unknown_weapon
     }
 }
